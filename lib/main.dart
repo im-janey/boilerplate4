506354672d6491f420784898b2cff9b7257dc762
im-janey <jane_seohyeon@handong.ac.kr>
2024-07-22 15:01:16 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         TextButton(
                           onPressed: () {},
                           child: Text(
-                            'CANCEL c',
+                            'CANCEL cj',
                             style: TextStyle(
                               fontSize: 12,
                               color: Theme.of(context).colorScheme.primary,
